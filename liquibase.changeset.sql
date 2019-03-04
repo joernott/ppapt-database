d9f510008b1f4aa2d9958659f2c6361710775c79
Joern Ott <joern.ott@ott-consult.de>
2019-03-04 12:19:52 +0000

    Fix SQL

--- liquibase.changeset.sql
+++ liquibase.changeset.sql
@@ -10,8 +10,7 @@ CREATE TABLE campaigns (
   game_round    int NOT NULL
 );
 
-ALTER TABLE campaigns
-  ADD PRIMARY KEY (campaign_id);
+ALTER TABLE campaigns ADD PRIMARY KEY (campaign_id, campaign_id);
 
 CREATE TABLE characters (
   character_id   varchar(36) NOT NULL,
@@ -21,19 +20,17 @@ CREATE TABLE characters (
   character_role int NOT NULL
 );
 
-ALTER TABLE characters
-  ADD PRIMARY KEY (character_id),
-  ADD KEY character_role (character_role),
-  ADD KEY character_user (character_user),
-  ADD KEY campaign (campaign);
+ALTER TABLE characters ADD PRIMARY KEY (character_id, character_id);
+CREATE INDEX ON characters.character_role;
+CREATE INDEX ON characters.character_user;
+CREATE INDEX ON characters.campaign;
 
 CREATE TABLE roles (
   role_id   int NOT NULL,
   role_name varchar(32) NOT NULL
 );
 
-ALTER TABLE roles
-  ADD PRIMARY KEY (role_id);
+ALTER TABLE roles ADD PRIMARY KEY (role_id, role_id);
 
 CREATE TABLE users (
   email         varchar(128) NOT NULL,
@@ -43,19 +40,18 @@ CREATE TABLE users (
 );
 
 
-ALTER TABLE users
-  ADD PRIMARY KEY (email);
+ALTER TABLE users ADD PRIMARY KEY (email, email);
 
 
 ALTER TABLE campaigns
-  ADD CONSTRAINT rel_campaigns_characters FOREIGN KEY (campaign_id) REFERENCES characters (campaign);
+  ADD CONSTRAINT rel_campaigns_characters FOREIGN KEY (campaign_id) REFERENCES characters (campaign) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 ALTER TABLE characters
-  ADD CONSTRAINT rel_characters_roles FOREIGN KEY (character_role) REFERENCES roles (role_id);
+  ADD CONSTRAINT rel_characters_roles FOREIGN KEY (character_role) REFERENCES roles (role_id)  ON UPDATE CASCADE ON DELETE RESTRICT;
 
 ALTER TABLE users
-  ADD CONSTRAINT rel_users_characters FOREIGN KEY (email) REFERENCES characters (character_user);
-COMMIT;
+  ADD CONSTRAINT rel_users_characters FOREIGN KEY (email) REFERENCES characters (character_user)  ON UPDATE CASCADE ON DELETE RESTRICT;
+
 
 --changeset joernott:2 failOnError=true
 
