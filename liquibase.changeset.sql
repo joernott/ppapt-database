b621361e3426b8e0e11a5a590c62ee832dabd653
Joern Ott <joern.ott@ott-consult.de>
2019-03-04 11:54:50 +0000

    Cleanup naming

--- liquibase.changeset.sql
+++ liquibase.changeset.sql
@@ -2,64 +2,64 @@
 
 --changeset joernott:1 failOnError=true
 
-CREATE TABLE campaign (
-  campaignid varchar(36) NOT NULL,
-  name varchar(128) NOT NULL,
-  startdate bigint NOT NULL,
-  campaignday bigint NOT NULL,
-  gameround int NOT NULL
+CREATE TABLE campaigns (
+  campaign_id   varchar(36) NOT NULL,
+  campaign_name varchar(128) NOT NULL,
+  startdate     bigint NOT NULL,
+  campaign_day  bigint NOT NULL,
+  game_round    int NOT NULL
 );
 
-ALTER TABLE campaign
-  ADD PRIMARY KEY (campaignid);
+ALTER TABLE campaigns
+  ADD PRIMARY KEY (campaign_id);
 
 CREATE TABLE characters (
-  characterid varchar(36) NOT NULL,
-  user varchar(128) NOT NULL,
-  campaign varchar(36) NOT NULL,
-  name varchar(128) NOT NULL,
-  role int NOT NULL
+  character_id   varchar(36) NOT NULL,
+  character_user varchar(128) NOT NULL,
+  campaign       varchar(36) NOT NULL,
+  character_name varchar(128) NOT NULL,
+  character_role int NOT NULL
 );
 
 ALTER TABLE characters
-  ADD PRIMARY KEY (characterid),
-  ADD KEY role (role),
-  ADD KEY user (user),
-  ADD KEY campaign (campaign);
+  ADD PRIMARY KEY (character_id),
+  ADD KEY character_role (roles),
+  ADD KEY character_user (users),
+  ADD KEY campaign (campaigns);
 
-CREATE TABLE role (
-  roleid int NOT NULL,
-  name varchar(32) NOT NULL
+CREATE TABLE roles (
+  role_id   int NOT NULL,
+  role_name varchar(32) NOT NULL
 );
 
-ALTER TABLE role
-  ADD PRIMARY KEY (roleid);
+ALTER TABLE roles
+  ADD PRIMARY KEY (role_id);
 
-CREATE TABLE user (
-  email varchar(128) NOT NULL,
-  username varchar(64) NOT NULL,
-  password varchar(60) NOT NULL,
-  locked boolean NOT NULL
+CREATE TABLE users (
+  email         varchar(128) NOT NULL,
+  user_name     varchar(64) NOT NULL,
+  user_password varchar(60) NOT NULL,
+  user_locked   boolean NOT NULL
 );
 
 
-ALTER TABLE user
+ALTER TABLE users
   ADD PRIMARY KEY (email);
 
 
 ALTER TABLE campaign
-  ADD CONSTRAINT rel_campaign_characters FOREIGN KEY (campaignid) REFERENCES characters (campaign);
+  ADD CONSTRAINT rel_campaigns_characters FOREIGN KEY (campaign_id) REFERENCES characters (campaign);
 
 ALTER TABLE characters
-  ADD CONSTRAINT rel_characters_role FOREIGN KEY (role) REFERENCES role (roleid);
+  ADD CONSTRAINT rel_characters_roles FOREIGN KEY (character_role) REFERENCES roles (role_id);
 
-ALTER TABLE user
-  ADD CONSTRAINT rel_user_characters FOREIGN KEY (email) REFERENCES characters (user);
+ALTER TABLE users
+  ADD CONSTRAINT rel_users_characters FOREIGN KEY (email) REFERENCES characters (character_user);
 COMMIT;
 
 --changeset joernott:2 failOnError=true
 
-INSERT INTO role (roleid, name) VALUES
+INSERT INTO roles (role_id, role_name) VALUES
 (0, 'Gamemaster'),
 (1, 'Player'),
 (2, 'Dead character'),
